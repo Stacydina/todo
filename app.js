d29084a9bd41c585015171bd0ff53e7d16dc94ac
Stacydina <stacydina6@gmail.com>
2021-01-06 19:39:53 +0300

    filter applied to tasks

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const totdoInput = document.querySelector(".todo-input");
 const totdoButton = document.querySelector(".todo-button");
+const filterOption= document.querySelector(".filter-todo")
 
 const totdoList = document.querySelector(".todo-list");
 
-totdoButton.addEventListener('click', addTodo)
-totdoList.addEventListener('click', deleteCheck)
-
+totdoButton.addEventListener('click', addTodo);
+totdoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click',filterTodo)
+;
 function addTodo(event) {
    event.preventDefault();
    
@@ -38,7 +40,11 @@ function deleteCheck(e) {
 
     if(item.classList[0] ==="trash-btn"){
         const todo =item.parentElement;
-        todo.remove();
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        })
+        
     }
 
     if(item.classList[0] === "complete-btn"){
@@ -46,4 +52,36 @@ function deleteCheck(e) {
         todo.classList.toggle('completed');
     }
     
+}
+
+function filterTodo(e){
+    const todos = totdoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display='flex';
+                break;
+                case "completed":
+                    if(todo.classList.contains("completed")){
+                        todo.style.display='flex';
+                    
+                    }else{
+                        todo.style.display ="none";
+                    }
+                    break;
+
+                    case "uncompleted":
+                        if(!todo.classList.contains("completed")){
+                            todo.style.display='flex';
+                        
+                        }else{
+                            todo.style.display ="none";
+                        }
+                        break;
+        
+        }
+
+        
+    })
+
 }
